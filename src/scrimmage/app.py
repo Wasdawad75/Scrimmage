178f2ba570c80cb373cda494c02b3c1e19df1cdf
Wasdawad75 <beeren1907@gmail.com>
2026-08-26 10:40:28 -0500

    Midway thru APIs

--- src/scrimmage/app.py
+++ src/scrimmage/app.py
@@ -1,87 +1,60 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from src.schemas import PlayerCreate
-
-app = FastAPI()
-
-players_ = [
-    {
-        "id": 3139477,
-        "full_name": "Patrick Mahomes",
-        "position": "Quarterback",
-        "jersey_number": 15,
-        "team": "Kansas City Chiefs",
-        "photo_url": "https://espncdn.com",
-        "height": "6-2",
-        "weight": 225,
-        "stats": {
-            "passing_yards": 4183,
-            "touchdowns": 27,
-            "interceptions": 14
-        }
-    },
-    {
-        "id": 3916387,
-        "full_name": "Lamar Jackson",
-        "position": "Quarterback",
-        "jersey_number": 8,
-        "team": "Baltimore Ravens",
-        "photo_url": "https://espncdn.com",
-        "height": "6-2",
-        "weight": 215,
-        "stats": {
-            "passing_yards": 3678,
-            "rushing_yards": 821,
-            "touchdowns": 29
-        }
-    },
-    {
-        "id": 4262921,
-        "full_name": "Justin Jefferson",
-        "position": "Wide Receiver",
-        "jersey_number": 18,
-        "team": "Minnesota Vikings",
-        "photo_url": "https://espncdn.com",
-        "height": "6-1",
-        "weight": 195,
-        "stats": {
-            "receptions": 68,
-            "receiving_yards": 1074,
-            "touchdowns": 5
-        }
-    },
-    {
-        "id": 3117251,
-        "full_name": "Christian McCaffrey",
-        "position": "Running Back",
-        "jersey_number": 23,
-        "team": "San Francisco 49ers",
-        "photo_url": "https://espncdn.com",
-        "height": "5-11",
-        "weight": 210,
-        "stats": {
-            "rushing_yards": 1459,
-            "receiving_yards": 564,
-            "total_touchdowns": 21
-        }
-    },
-    {
-        "id": 4360310,
-        "full_name": "Amon-Ra St. Brown",
-        "position": "Wide Receiver",
-        "jersey_number": 14,
-        "team": "Detroit Lions",
-        "photo_url": "https://espncdn.com",
-        "height": "6-0",
-        "weight": 202,
-        "stats": {
-            "receptions": 119,
-            "receiving_yards": 1515,
-            "touchdowns": 10
-        }
-    }
-]
+from src.scrimmage.db import Player, create_db_and_tables, get_async_session
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+
+@app.post("/upload")
+async def upload_file(file: UploadFile = File(...),
+    name: str = Form(""),
+    session: AsyncSession = Depends(get_async_session)):
+
+    player = Player(
+        name = name,
+        profile_pic = "dummypic",
+        file_type = "photo",
+        file_name="dummy_name",
+    )
+    session.add(player)
+    await session.commit()
+    await session.refresh(player)
+    return player
+
+
+#endpoint to view players
+@app.get("/statsheet")
+async def upload_statsheet(
+    session: AsyncSession = Depends(get_async_session)
+):
+    result = await session.execute(select(Player).order_by(Player.created_atdesc()))
+    players = [row[0] for row in result.all()]
+
+    players_data = []
+    for player in players:
+        players_data.append(
+            {
+                "id": (player.id),
+                "name": player.name,
+                "profile_pic": player.profile_pic,
+                "file_type": player.file_type,
+                "file_name": player.file_name,
+                "created_at": player.created_at.isoformat()
+
+            }
+        )
+
+
+"""
 @app.get("/players")
 
 def get_all_players(limit: int = None):
@@ -105,3 +78,6 @@ def create_post(post: PlayerCreate):
     new_player = post.model_dump()
     players_.append(new_player)
     return new_player
+
+
+#delete data"""
